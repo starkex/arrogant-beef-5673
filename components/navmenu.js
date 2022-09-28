d318d04df1e79982f7f2a68a72286537d1c3138b
starkex <starkanonimity511@gmail.com>
2022-09-29 00:07:30 +0530

    menu-import

---
+++ components/navmenu.js
@@ -0,0 +1,14 @@
+function NavMenu(){
+    return ` <ul class="menu">
+    <li class="active"><a >Home</a></li>
+    <li><a >Meal Deals</a></li>
+    <li><a >KindMoments</a></li>
+    <li><a >HotPicks</a></li>
+    <li><a >Recipies</a></li>
+    <li><a >Directory</a></li>
+    <li><a >Articles</a></li>
+    <li><a >Help</a></li>
+</ul>`
+}
+
+export default NavMenu;
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
 import { NavBar } from "../components/navbar.js";
+import NavMenu from "../components/navmenu.js";
 
 document.getElementById('navbar').innerHTML = NavBar()
+document.getElementById('nav-menu').innerHTML = NavMenu()
+
+let act = document.querySelectorAll('.menu li>a')
+console.log(act)
+for(let i=0;i<act.length;i++){
+    console.log(act)
+    act[i].onclick = function(){
+        let ive = document.querySelector('.menu li.active');
+        if(ive) ive.classList.remove('active');
+        this.parentNode.classList.add('active');
+    };
+}
