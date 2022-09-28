9dee9a1a847b10e5686d18f87827ed3d9265164d
starkex <starkanonimity511@gmail.com>
2022-09-29 01:23:57 +0530

    directory

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,8 @@ document.getElementById('navbar').innerHTML = NavBar()
 document.getElementById('nav-menu').innerHTML = NavMenu()
 
 let act = document.querySelectorAll('.menu li>a')
-console.log(act)
+// console.log(act)
 for(let i=0;i<act.length;i++){
-    console.log(act)
     act[i].onclick = function(){
         let ive = document.querySelector('.menu li.active');
         if(ive) ive.classList.remove('active');
