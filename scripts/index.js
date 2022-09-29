56e8f4d95ec37f397f698c985d1e3e9a5196066a
starkex <starkanonimity511@gmail.com>
2022-09-29 18:35:20 +0530

    latest-home

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,34 @@ for(let i=0;i<act.length;i++){
         this.parentNode.classList.add('active');
     };
 }
+
+const News = async ()=>{
+    let res = await fetch(`https://snacks-api1o1.herokuapp.com/elements/Snacks`)
+    let newsdata = await res.json();
+    // let newsimg=''
+    for(let i=0;i<5;i++){
+        let newsimg  = newsdata[i]['card__img src']
+        let newstitle  = newsdata[i]['card__title-text']
+        let img  = document.createElement('img')
+        img.src = newsimg;
+        
+        let title = document.createElement('p')
+        title.innerText = newstitle;
+
+        let blockdiv = document.createElement('div')
+        blockdiv.setAttribute('id','bdiv')
+        blockdiv.append(img,title)
+
+        document.getElementById('blocks').append(blockdiv)
+    }
+    console.log(newsdata)
+    // Displaynews(newsdata)
+}
+News()
+
+function Displaynews(data){
+    
+    data.forEach((el) => {
+            console.log(el['card__img src'])
+    });
+}
