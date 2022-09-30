a8a8374297d51c87c95a34c35e72f44a080a6c81
starkex <starkanonimity511@gmail.com>
2022-09-30 16:14:22 +0530

    footer-addtion

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
 import { NavBar } from "../components/navbar.js";
 import NavMenu from "../components/navmenu.js";
+import { Footer } from "../components/footer.js";
 
 document.getElementById('navbar').innerHTML = NavBar()
 document.getElementById('nav-menu').innerHTML = NavMenu()
+document.getElementById('ft-back').innerHTML = Footer()
 
 document.getElementById('web-logo').addEventListener('click',()=>{
 	window.location.href = 'index.html'
